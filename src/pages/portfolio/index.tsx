2925ebc119f3e5fac714d9213e776ae37c789e1e
Angelo Gustavo <angelotj43@gmail.com>
2022-11-09 23:53:35 -0300

    nova atualizacao

--- src/pages/portfolio/index.tsx
+++ src/pages/portfolio/index.tsx
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import React, { useEffect, useRef, useState } from "react";
+import React, { useState } from "react";
 import { NavbarHome } from "../../components/Navbar";
 import styles from "./styles.module.sass";
 import Link from "next/link";
